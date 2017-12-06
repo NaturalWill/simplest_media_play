70ba0c8b2ba4ce8256a4fa1e4b2a822d039b01af
Jier <naturalwill999@gmail.com>
2017-12-06 10:10:23 +0800

    upgrade solution, add WPF use d3d9 sample

---
+++ WpfDirect3DDemo/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDirect3DDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("simplest_video_play_direct3d.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr Init(IntPtr resourcePointer, UInt32 width, UInt32 height);
+
+        [DllImport("simplest_video_play_direct3d.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Play(IntPtr resourcePointer);
+
+        [DllImport("simplest_video_play_direct3d.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool Render();
+        [DllImport("simplest_video_play_direct3d.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Cleanup();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Play(new WindowInteropHelper(this).Handle);
+            initialize();
+        }
+        #region D3D
+        private void initialize()
+        {
+            //    vm.LiveConfigList[0].Initialize(new WindowInteropHelper(this).Handle,
+            //               (int)ImageHost.ActualWidth, (int)ImageHost.ActualHeight);
+            IntPtr surface =
+            Init(new WindowInteropHelper(this).Handle, (uint)gImageHost.ActualWidth, (uint)gImageHost.ActualHeight);
+
+            if (surface != IntPtr.Zero)
+            {
+                d3dImage.Lock();
+                d3dImage.SetBackBuffer(D3DResourceType.IDirect3DSurface9, surface);
+                d3dImage.Unlock();
+
+                CompositionTarget.Rendering += CompositionTarget_Rendering;
+            }
+        }
+
+        private void UninitializeRendering()
+        {
+
+            CompositionTarget.Rendering -= this.CompositionTarget_Rendering;
+        }
+
+        private void CompositionTarget_Rendering(object sender, EventArgs e)
+        {
+            if (d3dImage.IsFrontBufferAvailable)
+            {
+                d3dImage.Lock();
+                Render();
+                // Invalidate the whole area:
+                d3dImage.AddDirtyRect(new Int32Rect(0, 0, d3dImage.PixelWidth, d3dImage.PixelHeight));
+                d3dImage.Unlock();
+            }
+        }
+        private void d3dImage_IsFrontBufferAvailableChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (d3dImage.IsFrontBufferAvailable)
+            {
+                initialize();
+            }
+            else
+            {
+                // Cleanup:
+                UninitializeRendering();
+                Cleanup();
+            }
+        }
+        #endregion
+    }
+}
--- simplest_video_play_direct3d/simplest_video_play_direct3d.cpp
+++ simplest_video_play_direct3d/simplest_video_play_direct3d.cpp
@@ -52,114 +52,138 @@
 #include <stdio.h>
 #include <tchar.h>
 #include <d3d9.h>
+#pragma comment(lib,"d3d9.lib") 
+
+
+#define LIBCRLIVE_EXPORT_API
+#ifdef LIBCRLIVE_EXPORT_API
+#define EXPORT_API __declspec(dllexport)
+#else
+#define EXPORT_API 
+#endif
 
 CRITICAL_SECTION  m_critial;
 
-IDirect3D9 *m_pDirect3D9= NULL;
-IDirect3DDevice9 *m_pDirect3DDevice= NULL;
-IDirect3DSurface9 *m_pDirect3DSurfaceRender= NULL;
+IDirect3D9 *m_pDirect3D9 = NULL;
+IDirect3DDevice9 *m_pDirect3DDevice = NULL;
+IDirect3DSurface9 *m_pDirect3DSurfaceRender = NULL;
 
 RECT m_rtViewport;
 
 //set '1' to choose a type of file to play
 //Read BGRA data
-#define LOAD_BGRA    0
+#define LOAD_BGRA    1
 //Read YUV420P data
-#define LOAD_YUV420P 1
+#define LOAD_YUV420P 0
+#define LOAD_YUYV422 0
 
 
 //Width, Height
-const int screen_w=500,screen_h=500;
-const int pixel_w=320,pixel_h=180;
-FILE *fp=NULL;
+const int screen_w = 500, screen_h = 500;
+const int pixel_w = 320, pixel_h = 180;
+FILE *fp = NULL;
 
 //Bit per Pixel
 #if LOAD_BGRA
-const int bpp=32;
+const int bpp = 32;
 #elif LOAD_YUV420P
-const int bpp=12;
+const int bpp = 12;
+#elif LOAD_YUYV422
+const int bpp = 12;
 #endif
 
-unsigned char buffer[pixel_w*pixel_h*bpp/8];
+unsigned char buffer[pixel_w*pixel_h*bpp / 8];
 
 
-void Cleanup()
+extern "C" EXPORT_API void Cleanup()
 {
 	EnterCriticalSection(&m_critial);
-	if(m_pDirect3DSurfaceRender)
+	if (m_pDirect3DSurfaceRender)
 		m_pDirect3DSurfaceRender->Release();
-	if(m_pDirect3DDevice)
+	if (m_pDirect3DDevice)
 		m_pDirect3DDevice->Release();
-	if(m_pDirect3D9)
+	if (m_pDirect3D9)
 		m_pDirect3D9->Release();
 	LeaveCriticalSection(&m_critial);
 }
 
 
-int InitD3D( HWND hwnd, unsigned long lWidth, unsigned long lHeight )
+int InitD3D(HWND hwnd, unsigned long lWidth, unsigned long lHeight)
 {
 	HRESULT lRet;
 	InitializeCriticalSection(&m_critial);
 	Cleanup();
 
-	m_pDirect3D9 = Direct3DCreate9( D3D_SDK_VERSION );
-	if( m_pDirect3D9 == NULL )
+	m_pDirect3D9 = Direct3DCreate9(D3D_SDK_VERSION);
+	if (m_pDirect3D9 == NULL)
 		return -1;
 
-	D3DPRESENT_PARAMETERS d3dpp; 
-	ZeroMemory( &d3dpp, sizeof(d3dpp) );
+	D3DPRESENT_PARAMETERS d3dpp;
+	ZeroMemory(&d3dpp, sizeof(d3dpp));
 	d3dpp.Windowed = TRUE;
 	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
 	d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
 
-	GetClientRect(hwnd,&m_rtViewport);
+	GetClientRect(hwnd, &m_rtViewport);
 
-	lRet=m_pDirect3D9->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL,hwnd,
+	lRet = m_pDirect3D9->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hwnd,
 		D3DCREATE_SOFTWARE_VERTEXPROCESSING,
-		&d3dpp, &m_pDirect3DDevice );
-	if(FAILED(lRet))
+		&d3dpp, &m_pDirect3DDevice);
+	if (FAILED(lRet))
 		return -1;
 
+	// Turn on the zbuffer
+	m_pDirect3DDevice->SetRenderState(D3DRS_ZENABLE, TRUE);
+
+	// Turn on ambient lighting 
+	m_pDirect3DDevice->SetRenderState(D3DRS_AMBIENT, 0xffffffff);
+
 #if LOAD_BGRA
-	lRet=m_pDirect3DDevice->CreateOffscreenPlainSurface(
-		lWidth,lHeight,
+	lRet = m_pDirect3DDevice->CreateOffscreenPlainSurface(
+		lWidth, lHeight,
 		D3DFMT_X8R8G8B8,
 		D3DPOOL_DEFAULT,
 		&m_pDirect3DSurfaceRender,
 		NULL);
 #elif LOAD_YUV420P
-	lRet=m_pDirect3DDevice->CreateOffscreenPlainSurface(
-		lWidth,lHeight,
+	//lRet = m_pDirect3DDevice->CreateOffscreenPlainSurface(
+	//	lWidth, lHeight,
+	//	(D3DFORMAT)MAKEFOURCC('Y', 'V', '1', '2'),
+	//	D3DPOOL_DEFAULT,
+	//	&m_pDirect3DSurfaceRender,
+	//	NULL);
+
+	lRet = m_pDirect3DDevice->CreateRenderTarget(lWidth, lHeight,
 		(D3DFORMAT)MAKEFOURCC('Y', 'V', '1', '2'),
-		D3DPOOL_DEFAULT,
-		&m_pDirect3DSurfaceRender,
-		NULL);
+		D3DMULTISAMPLE_NONE, 0,
+		true, // lockable (true for compatibility with Windows XP.  False is preferred for Windows Vista or later)
+		&m_pDirect3DSurfaceRender, NULL);
 #endif
 
 
-	if(FAILED(lRet))
+	if (FAILED(lRet))
 		return -1;
 
 	return 0;
 }
 
 
-bool Render()
+extern "C" EXPORT_API bool Render()
 {
 	HRESULT lRet;
 	//Read Data
 	//RGB
-	if (fread(buffer, 1, pixel_w*pixel_h*bpp/8, fp) != pixel_w*pixel_h*bpp/8){
+	if (fread(buffer, 1, pixel_w*pixel_h*bpp / 8, fp) != pixel_w*pixel_h*bpp / 8) {
 		// Loop
 		fseek(fp, 0, SEEK_SET);
-		fread(buffer, 1, pixel_w*pixel_h*bpp/8, fp);
+		fread(buffer, 1, pixel_w*pixel_h*bpp / 8, fp);
 	}
-	
-	if(m_pDirect3DSurfaceRender == NULL)
+
+	if (m_pDirect3DSurfaceRender == NULL)
 		return -1;
 	D3DLOCKED_RECT d3d_rect;
-	lRet=m_pDirect3DSurfaceRender->LockRect(&d3d_rect,NULL,D3DLOCK_DONOTWAIT);
-	if(FAILED(lRet))
+	lRet = m_pDirect3DSurfaceRender->LockRect(&d3d_rect, NULL, D3DLOCK_DONOTWAIT);
+	if (FAILED(lRet))
 		return -1;
 
 	byte *pSrc = buffer;
@@ -169,40 +193,56 @@ bool Render()
 
 	//Copy Data
 #if LOAD_BGRA
-	int pixel_w_size=pixel_w*4;
-	for(i=0; i< pixel_h; i++){
-		memcpy( pDest, pSrc, pixel_w_size );
+	int pixel_w_size = pixel_w * 4;
+	for (i = 0; i < pixel_h; i++) {
+		memcpy(pDest, pSrc, pixel_w_size);
 		pDest += stride;
 		pSrc += pixel_w_size;
 	}
 #elif LOAD_YUV420P
-	for(i = 0;i < pixel_h;i ++){
-		memcpy(pDest + i * stride,pSrc + i * pixel_w, pixel_w);
+	for (i = 0; i < pixel_h; i++) {
+		memcpy(pDest + i * stride, pSrc + i * pixel_w, pixel_w);
 	}
-	for(i = 0;i < pixel_h/2;i ++){
-		memcpy(pDest + stride * pixel_h + i * stride / 2,pSrc + pixel_w * pixel_h + pixel_w * pixel_h / 4 + i * pixel_w / 2, pixel_w / 2);
+	for (i = 0; i < pixel_h / 2; i++) {
+		memcpy(pDest + stride * pixel_h + i * stride / 2, pSrc + pixel_w * pixel_h + pixel_w * pixel_h / 4 + i * pixel_w / 2, pixel_w / 2);
 	}
-	for(i = 0;i < pixel_h/2;i ++){
-		memcpy(pDest + stride * pixel_h + stride * pixel_h / 4 + i * stride / 2,pSrc + pixel_w * pixel_h + i * pixel_w / 2, pixel_w / 2);
+	for (i = 0; i < pixel_h / 2; i++) {
+		memcpy(pDest + stride * pixel_h + stride * pixel_h / 4 + i * stride / 2, pSrc + pixel_w * pixel_h + i * pixel_w / 2, pixel_w / 2);
+	}
+#elif LOAD_YUYV422
+	for (i = 0; i < pixel_h; i++) {
+		memcpy(pDest + i * stride,
+			pSrc + i * pixel_w,
+			pixel_w);
+	}
+	for (i = 0; i < pixel_h / 2; i++) {
+		memcpy(pDest + stride * pixel_h + i * stride / 2, 
+			pSrc + pixel_w * pixel_h + pixel_w * pixel_h / 4 + i * pixel_w / 2,
+			pixel_w / 2);
+	}
+	for (i = 0; i < pixel_h / 2; i++) {
+		memcpy(pDest + stride * pixel_h + stride * pixel_h / 4 + i * stride / 2,
+			pSrc + pixel_w * pixel_h + i * pixel_w / 2, 
+			pixel_w / 2);
 	}
 #endif
 
-	lRet=m_pDirect3DSurfaceRender->UnlockRect();
-	if(FAILED(lRet))
+	lRet = m_pDirect3DSurfaceRender->UnlockRect();
+	if (FAILED(lRet))
 		return -1;
 
 	if (m_pDirect3DDevice == NULL)
 		return -1;
 
-	m_pDirect3DDevice->Clear( 0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0,0,0), 1.0f, 0 );
+	m_pDirect3DDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
 	m_pDirect3DDevice->BeginScene();
 	IDirect3DSurface9 * pBackBuffer = NULL;
 
-	m_pDirect3DDevice->GetBackBuffer(0,0,D3DBACKBUFFER_TYPE_MONO,&pBackBuffer);
-	m_pDirect3DDevice->StretchRect(m_pDirect3DSurfaceRender,NULL,pBackBuffer,&m_rtViewport,D3DTEXF_LINEAR);
+	m_pDirect3DDevice->GetBackBuffer(0, 0, D3DBACKBUFFER_TYPE_MONO, &pBackBuffer);
+	m_pDirect3DDevice->StretchRect(m_pDirect3DSurfaceRender, NULL, pBackBuffer, &m_rtViewport, D3DTEXF_LINEAR);
 	m_pDirect3DDevice->EndScene();
-	m_pDirect3DDevice->Present( NULL, NULL, NULL, NULL );
-	pBackBuffer->Release();
+	//m_pDirect3DDevice->Present(NULL, NULL, NULL, NULL);
+
 
 	return true;
 }
@@ -210,7 +250,7 @@ bool Render()
 
 LRESULT WINAPI MyWndProc(HWND hwnd, UINT msg, WPARAM wparma, LPARAM lparam)
 {
-	switch(msg){
+	switch (msg) {
 	case WM_DESTROY:
 		Cleanup();
 		PostQuitMessage(0);
@@ -219,7 +259,7 @@ LRESULT WINAPI MyWndProc(HWND hwnd, UINT msg, WPARAM wparma, LPARAM lparam)
 	return DefWindowProc(hwnd, msg, wparma, lparam);
 }
 
-int WINAPI WinMain( __in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, __in LPSTR lpCmdLine, __in int nShowCmd )
+int WINAPI WinMain(__in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, __in LPSTR lpCmdLine, __in int nShowCmd)
 {
 	WNDCLASSEX wc;
 	ZeroMemory(&wc, sizeof(wc));
@@ -233,12 +273,12 @@ int WINAPI WinMain( __in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, 
 	RegisterClassEx(&wc);
 
 	HWND hwnd = NULL;
-	hwnd = CreateWindow(L"D3D", L"Simplest Video Play Direct3D (Surface)", WS_OVERLAPPEDWINDOW, 100, 100, screen_w, screen_h, NULL, NULL, hInstance, NULL);
-	if (hwnd==NULL){
+	hwnd = CreateWindow(L"D3D", L"Simplest Video Play Direct3D (Surface)", WS_OVERLAPPEDWINDOW, 100, 100, 500, 500, NULL, NULL, hInstance, NULL);
+	if (hwnd == NULL) {
 		return -1;
 	}
-	
-	if(InitD3D( hwnd, pixel_w, pixel_h)==E_FAIL){
+
+	if (InitD3D(hwnd, pixel_w, pixel_h) == E_FAIL) {
 		return -1;
 	}
 
@@ -246,11 +286,11 @@ int WINAPI WinMain( __in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, 
 	UpdateWindow(hwnd);
 
 #if LOAD_BGRA
-	fp=fopen("../test_bgra_320x180.rgb","rb+");
+	fp = fopen("../test_bgra_320x180.rgb", "rb+");
 #elif LOAD_YUV420P
-	fp=fopen("../test_yuv420p_320x180.yuv","rb+");
+	fp = fopen("../test_yuv420p_320x180.yuv", "rb+");
 #endif
-	if(fp==NULL){
+	if (fp == NULL) {
 		printf("Cannot open this file.\n");
 		return -1;
 	}
@@ -258,13 +298,13 @@ int WINAPI WinMain( __in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, 
 	MSG msg;
 	ZeroMemory(&msg, sizeof(msg));
 
-	while (msg.message != WM_QUIT){
+	while (msg.message != WM_QUIT) {
 		//PeekMessage, not GetMessage
-		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)){
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
-		else{
+		else {
 			Sleep(40);
 			Render();
 		}
@@ -275,3 +315,156 @@ int WINAPI WinMain( __in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, 
 	return 0;
 }
 
+
+extern "C" EXPORT_API IDirect3DSurface9* Init(HWND hwnd,
+	unsigned long lWidth,
+	unsigned long lHeight)
+{
+	if (fp == NULL) {
+#if LOAD_BGRA
+		fp = fopen("../test_bgra_320x180.rgb", "rb+");
+#elif LOAD_YUV420P
+
+		fp = fopen("../test_yuv420p_320x180.yuv", "rb+");
+#elif LOAD_YUYV422
+
+		fp = fopen("../test_yuyv422_320x180.yuv", "rb+");
+#endif
+	}
+	//if (fp == NULL) {
+	//	printf("Cannot open this file.\n");
+	//	return -1;
+	//}
+
+
+	HRESULT lRet;
+	InitializeCriticalSection(&m_critial);
+	Cleanup();
+
+	m_pDirect3D9 = Direct3DCreate9(D3D_SDK_VERSION);
+	if (m_pDirect3D9 == NULL)
+		return nullptr;
+
+	//D3DPRESENT_PARAMETERS d3dpp;
+	//ZeroMemory(&d3dpp, sizeof(d3dpp));
+	//d3dpp.Windowed = TRUE;
+	//d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	//d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+
+
+
+	// Set up the structure used to create the D3DDevice. Since we are now
+	// using more complex geometry, we will create a device with a zbuffer.
+	D3DPRESENT_PARAMETERS d3dpp;
+	ZeroMemory(&d3dpp, sizeof(d3dpp));
+	d3dpp.Windowed = TRUE;
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+	d3dpp.EnableAutoDepthStencil = TRUE;
+	d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
+
+	GetClientRect(hwnd, &m_rtViewport);
+
+	lRet = m_pDirect3D9->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hwnd,
+		D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+		&d3dpp, &m_pDirect3DDevice);
+	if (FAILED(lRet))
+		return nullptr;
+
+	// Turn on the zbuffer
+	m_pDirect3DDevice->SetRenderState(D3DRS_ZENABLE, TRUE);
+
+	// Turn on ambient lighting 
+	m_pDirect3DDevice->SetRenderState(D3DRS_AMBIENT, 0xffffffff);
+
+#if LOAD_BGRA
+	//lRet = m_pDirect3DDevice->CreateOffscreenPlainSurface(
+	//	lWidth, lHeight,
+	//	D3DFMT_X8R8G8B8,
+	//	D3DPOOL_DEFAULT,
+	//	&m_pDirect3DSurfaceRender,
+	//	NULL);
+
+	lRet = m_pDirect3DDevice->CreateRenderTarget(
+		lWidth, lHeight,
+		D3DFMT_X8R8G8B8,
+		D3DMULTISAMPLE_NONE, 0,
+		true, // lockable (true for compatibility with Windows XP.  False is preferred for Windows Vista or later)
+		&m_pDirect3DSurfaceRender, NULL);
+#elif LOAD_YUV420P
+	//lRet = m_pDirect3DDevice->CreateOffscreenPlainSurface(
+	//	lWidth, lHeight,
+	//	(D3DFORMAT)MAKEFOURCC('Y', 'V', '1', '2'),
+	//	D3DPOOL_DEFAULT,
+	//	&m_pDirect3DSurfaceRender,
+	//	NULL);
+
+	lRet = m_pDirect3DDevice->CreateRenderTarget(lWidth, lHeight,
+		(D3DFORMAT)MAKEFOURCC('Y', 'V', '1', '2'),
+		D3DMULTISAMPLE_NONE, 0,
+		true, // lockable (true for compatibility with Windows XP.  False is preferred for Windows Vista or later)
+		&m_pDirect3DSurfaceRender, NULL);
+
+#elif LOAD_YUYV422
+	//lRet = m_pDirect3DDevice->CreateOffscreenPlainSurface(
+	//	lWidth, lHeight,
+	//	(D3DFORMAT)MAKEFOURCC('Y', 'V', '1', '2'),
+	//	D3DPOOL_DEFAULT,
+	//	&m_pDirect3DSurfaceRender,
+	//	NULL);
+
+	lRet = m_pDirect3DDevice->CreateRenderTarget(lWidth, lHeight,
+		D3DFMT_YUY2,
+		D3DMULTISAMPLE_NONE, 0,
+		true, // lockable (true for compatibility with Windows XP.  False is preferred for Windows Vista or later)
+		&m_pDirect3DSurfaceRender, NULL);
+#endif
+
+
+	//m_pDirect3DDevice->SetRenderTarget(0, m_pDirect3DSurfaceRender);
+
+	return m_pDirect3DSurfaceRender;
+}
+
+
+
+extern "C" EXPORT_API int  Play(HWND hwnd)
+{
+
+	if (hwnd == NULL) {
+		return -1;
+	}
+
+	if (InitD3D(hwnd, pixel_w, pixel_h) == E_FAIL) {
+		return -1;
+	}
+
+
+#if LOAD_BGRA
+	fp = fopen("../test_bgra_320x180.rgb", "rb+");
+#elif LOAD_YUV420P
+	fp = fopen("../test_yuv420p_320x180.yuv", "rb+");
+#endif
+	if (fp == NULL) {
+		printf("Cannot open this file.\n");
+		return -1;
+	}
+
+	MSG msg;
+	ZeroMemory(&msg, sizeof(msg));
+
+	while (msg.message != WM_QUIT) {
+		//PeekMessage, not GetMessage
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else {
+			Sleep(40);
+			Render();
+		}
+	}
+
+
+	return 0;
+}
